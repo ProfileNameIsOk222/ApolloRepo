3a775a6dab1b40ac3ba0b7ea49c7a77f47b71f4f
ProfileNameIsOk222 <jrosalia07@fastmail.com>
2025-06-30 19:16:00 -0400

    Add files via upload

---
+++ asteroid-draw/build/src/CMakeFiles/asteroid-draw.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/profilenameisok/asteroid-draw/build/src/asteroid-draw_autogen/EWIEGA46WW/qrc_resources.cpp" "src/CMakeFiles/asteroid-draw.dir/asteroid-draw_autogen/EWIEGA46WW/qrc_resources.cpp.o" "gcc" "src/CMakeFiles/asteroid-draw.dir/asteroid-draw_autogen/EWIEGA46WW/qrc_resources.cpp.o.d"
+  "/home/profilenameisok/asteroid-draw/build/src/asteroid-draw_autogen/mocs_compilation.cpp" "src/CMakeFiles/asteroid-draw.dir/asteroid-draw_autogen/mocs_compilation.cpp.o" "gcc" "src/CMakeFiles/asteroid-draw.dir/asteroid-draw_autogen/mocs_compilation.cpp.o.d"
+  "/home/profilenameisok/asteroid-draw/src/main.cpp" "src/CMakeFiles/asteroid-draw.dir/main.cpp.o" "gcc" "src/CMakeFiles/asteroid-draw.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
